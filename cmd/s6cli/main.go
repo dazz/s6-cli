1e99aaa7b9025a3604e91d4b4a27c17680da1c69
dazz <annejulia@gmail.com>
2023-12-20 00:34:38 +0100

    create and remove a oneshot service

--- cmd/s6cli/main.go
+++ cmd/s6cli/main.go
@@ -1,111 +1,184 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "time"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-    "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 
-    "github.com/dazz/s6-cli/pkg/s6cli"
+	"github.com/dazz/s6-cli/pkg/s6cli"
 )
 
 func init() {
 }
 
 func main() {
-    app := &cli.App{
-        Name:  "s6-cli",
-        Version: "0.0.1",
-        Compiled: time.Now(),
-        Authors: []*cli.Author{
-            &cli.Author{
-                Name:  "Anne-Julia Seitz",
-                Email: "dazz@c-base",
-            },
-        },
-        Usage: "CLI for creating and linting files and directories",
-        // We'll be using the same flag for all our commands
-        // so we'll define it up here
-        Flags: []cli.Flag{
-            &cli.StringFlag{
-                Name:    "path",
-                Aliases: []string{"p"},
-                Value:   "/etc/s6-overlay/s6-rc.d",
-                Usage:   "Path to s6-rc.d directory",
-            },
-        },
-        Commands: []*cli.Command{
-            {
-                Name:    "lint",
-                Aliases: []string{"l"},
-                Usage:   "lint directories and files",
-                Action: func(cCtx *cli.Context) error {
-                    path := "/etc/s6-overlay/s6-rc.d"
-                    firstBundle := "user"
+	app := &cli.App{
+		Name:     "s6-cli",
+		Version:  "0.0.1",
+		Compiled: time.Now(),
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name:  "Anne-Julia Seitz",
+				Email: "dazz@c-base",
+			},
+		},
+		Usage: "CLI for creating and linting files and directories",
+		// We'll be using the same flag for all our commands
+		// so we'll define it up here
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "path",
+				Aliases: []string{"p"},
+				Value:   "/etc/s6-overlay/s6-rc.d",
+				Usage:   "Path to s6-rc.d directory",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "lint",
+				Aliases: []string{"l"},
+				Usage:   "lint directories and files",
+				Action: func(cCtx *cli.Context) error {
+					path := "/etc/s6-overlay/s6-rc.d"
+					firstBundle := "user"
 
-                    if cCtx.IsSet("path") {
-                        path = cCtx.String("path")
-                    }
-                    // check if the directory exists
-                    if _, err := os.Stat(path); os.IsNotExist(err) {
-                        fmt.Printf("Directory %s does not exist\n", path)
-                        os.Exit(1)
-                    }
+					if cCtx.IsSet("path") {
+						path = cCtx.String("path")
+					}
+					// check if the directory exists
+					if _, err := os.Stat(path); os.IsNotExist(err) {
+						fmt.Printf("Directory %s does not exist\n", path)
+						os.Exit(1)
+					}
 
-                    // compile dependency tree
-                    var services []s6cli.Service
-                    var lints []s6cli.Lint
-                    valid := s6cli.Compile(path, firstBundle, &services, &lints)
+					// compile dependency tree
+					var services []s6cli.Service
+					var lints []s6cli.Lint
+					valid := s6cli.Compile(path, firstBundle, &services, &lints)
 
-                    fmt.Println("*************** s6-cli Lint Report ***************")
+					fmt.Println("*************** s6-cli Lint Report ***************")
 
-                    // print lints
-                    for _, lint := range lints {
-                        fmt.Printf("* %s: %s\n", lint.Service, lint.Message)
-                    }
+					// print lints
+					for _, lint := range lints {
+						fmt.Printf("* %s: %s\n", lint.Service, lint.Message)
+					}
 
-                    fmt.Println("*************** s6-cli Lint Report ***************")
+					fmt.Println("*************** s6-cli Lint Report ***************")
 
-                    if valid {
-                        return nil
-                    }
-                    os.Exit(1)
-                    return nil
-                },
-            },
-            {
-                Name:    "mermaid",
-                Aliases: []string{"m"},
-                Usage:   "document s6 service dependencies in mermaid syntax",
-                Action: func(cCtx *cli.Context) error {
-                    path := "/etc/s6-overlay/s6-rc.d"
-                    firstBundle := "user"
+					if valid {
+						return nil
+					}
+					os.Exit(1)
+					return nil
+				},
+			},
+			{
+				Name:    "mermaid",
+				Aliases: []string{"m"},
+				Usage:   "document s6 service dependencies in mermaid syntax",
+				Action: func(cCtx *cli.Context) error {
+					path := "/etc/s6-overlay/s6-rc.d"
+					firstBundle := "user"
 
-                    if cCtx.IsSet("path") {
-                        path = cCtx.String("path")
-                    }
-                    // check if the directory exists
-                    if _, err := os.Stat(path); os.IsNotExist(err) {
-                        fmt.Printf("Directory %s does not exist\n", path)
-                        os.Exit(1)
-                    }
+					if cCtx.IsSet("path") {
+						path = cCtx.String("path")
+					}
+					// check if the directory exists
+					if _, err := os.Stat(path); os.IsNotExist(err) {
+						fmt.Printf("Directory %s does not exist\n", path)
+						os.Exit(1)
+					}
 
-                    // compile dependency tree
-                    var services []s6cli.Service
-                    var lints []s6cli.Lint
-                    s6cli.Compile(path, firstBundle, &services, &lints)
+					// compile dependency tree
+					var services []s6cli.Service
+					var lints []s6cli.Lint
+					s6cli.Compile(path, firstBundle, &services, &lints)
 
-                    fmt.Printf(s6cli.MermaidGraph(services))
+					fmt.Printf(s6cli.MermaidGraph(services))
 
-                    return nil
-                },
-            },
-        },
-    }
+					return nil
+				},
+			},
+			{
+				Name:      "create",
+				Aliases:   []string{"c"},
+				Usage:     "create a service",
+				ArgsUsage: "[type: (o|l|b)] [name]",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{Value: false, Name: "overwrite", Aliases: []string{"o"}, Usage: "Ignore existing files and directories"},
+				},
+				Action: func(cCtx *cli.Context) error {
+					path := "/etc/s6-overlay/s6-rc.d"
+					if cCtx.IsSet("path") {
+						path = cCtx.String("path")
+					}
+					// check if the directory exists
+					if _, err := os.Stat(path); os.IsNotExist(err) {
+						fmt.Printf("Directory %s does not exist\n", path)
+						os.Exit(1)
+					}
 
-    if err := app.Run(os.Args); err != nil {
-        log.Fatal(err)
-    }
+					serviceType := ""
+					if cCtx.Args().Get(0) == "o" || cCtx.Args().Get(0) == "l" || cCtx.Args().Get(0) == "b" {
+						serviceType = cCtx.Args().Get(0)
+					} else {
+						fmt.Print("Arg type must not be empty and one of 'o', 'l' or 'b'\n")
+						os.Exit(1)
+					}
+
+					name := ""
+					if cCtx.Args().Get(1) != "" {
+						name = cCtx.Args().Get(1)
+					} else {
+						fmt.Print("Arg name must not be empty\n")
+						os.Exit(1)
+					}
+
+					fmt.Printf("Create a service %s with type %s\n", name, serviceType)
+
+					if serviceType == "o" {
+						s6cli.Oneshot(path, name, []string{"base"})
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:      "remove",
+				Aliases:   []string{"rm"},
+				Usage:     "remove a service",
+				ArgsUsage: "[name]",
+				Action: func(cCtx *cli.Context) error {
+					path := "/etc/s6-overlay/s6-rc.d"
+					if cCtx.IsSet("path") {
+						path = cCtx.String("path")
+					}
+					// check if the directory exists
+					if _, err := os.Stat(path); os.IsNotExist(err) {
+						fmt.Printf("Directory %s does not exist\n", path)
+						os.Exit(1)
+					}
+
+					name := ""
+					if cCtx.Args().Get(0) != "" {
+						name = cCtx.Args().Get(0)
+					} else {
+						fmt.Print("Arg name must not be empty\n")
+						os.Exit(1)
+					}
+
+					s6cli.Remove(path, name)
+
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
--- pkg/s6cli/compile.go
+++ pkg/s6cli/compile.go
@@ -1,164 +1,162 @@
 package s6cli
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
-
-// Define a struct named Person
+// Service is a struct for a service
 type Service struct {
-    Name string
-    Type string
-    Dependencies []string
-}
-type Lint struct {
-    Service string
-    Message string
+	Name         string
+	Type         string
+	Dependencies []string
 }
 
+// Lint is a struct for a lint
+type Lint struct {
+	Service string
+	Message string
+}
 
 func Compile(rootPath string, currentService string, services *[]Service, lints *[]Lint) bool {
-    // Check if the service is already in the services list
-    if containsService(*services, currentService) {
-        return true
-    }
+	// Check if the service is already in the services list
+	if containsService(*services, currentService) {
+		return true
+	}
 
-    isValid := true
+	isValid := true
 
-    // Check if the directory exists
-    currentServicePath := rootPath + "/" + currentService
-    if _, err := os.Stat(currentServicePath); os.IsNotExist(err) {
-        *lints = append(*lints, Lint{
-            Service: currentService,
-            Message: fmt.Sprintf("directory for %s does not exist", currentService),
-        })
-        return false
-    }
+	// Check if the directory exists
+	currentServicePath := rootPath + "/" + currentService
+	if _, err := os.Stat(currentServicePath); os.IsNotExist(err) {
+		*lints = append(*lints, Lint{
+			Service: currentService,
+			Message: fmt.Sprintf("directory for %s does not exist", currentService),
+		})
+		return false
+	}
 
-    if currentService == "user" {}
+	// check the type file and content
+	typeFile := currentServicePath + "/type"
+	typeFileContent, err := os.ReadFile(typeFile)
+	if err != nil {
+		*lints = append(*lints, Lint{
+			Service: currentService,
+			Message: fmt.Sprintf("type file for \"%s\" does not exist", currentService),
+		})
+		return false
+	}
+	// Check if the file is empty
+	if len(typeFileContent) == 0 {
+		*lints = append(*lints, Lint{
+			Service: currentService,
+			Message: fmt.Sprintf("type file for \"%s\" is empty", currentService),
+		})
+		return false
+	}
 
-    // check the type file and content
-    typeFile := currentServicePath + "/type"
-    typeFileContent, err := ioutil.ReadFile(typeFile)
-    if err != nil {
-        *lints = append(*lints, Lint{
-            Service: currentService,
-            Message: fmt.Sprintf("type file for \"%s\" does not exist", currentService),
-        })
-        return false
-    }
-    // Check if the file is empty
-    if len(typeFileContent) == 0 {
-        *lints = append(*lints, Lint{
-            Service: currentService,
-            Message: fmt.Sprintf("type file for \"%s\" is empty", currentService),
-        })
-        return false
-    }
+	// Check if the last character is a newline
+	lastChar := typeFileContent[len(typeFileContent)-1]
+	if lastChar != '\n' {
+		*lints = append(*lints, Lint{
+			Service: currentService,
+			Message: fmt.Sprintf("type file for \"%s\" does not end with a newline", currentService),
+		})
+		isValid = false
+	}
 
-    // Check if the last character is a newline
-    lastChar := typeFileContent[len(typeFileContent)-1]
-    if lastChar != '\n' {
-        *lints = append(*lints, Lint{
-            Service: currentService,
-            Message: fmt.Sprintf("type file for \"%s\" does not end with a newline", currentService),
-        })
-        isValid = false
-    }
+	serviceType := strings.ReplaceAll(string(typeFileContent), "\n", "")
 
-    serviceType := strings.ReplaceAll(string(typeFileContent), "\n", "")
+	// check if the dependency directory exists
+	dependenciesDir := ""
+	if serviceType == "bundle" {
+		dependenciesDir = currentServicePath + "/contents.d"
+	} else if serviceType == "oneshot" || serviceType == "longrun" {
+		dependenciesDir = currentServicePath + "/dependencies.d"
+	} else {
+		*lints = append(*lints, Lint{
+			Service: currentService,
+			Message: fmt.Sprintf("service type (%s) in type file for %s does not exist", serviceType, currentService),
+		})
+		return false
+	}
 
-    // check if the dependency directory exists
-    dependenciesDir := ""
-    if serviceType == "bundle" {
-        dependenciesDir = currentServicePath + "/contents.d"
-    } else if serviceType == "oneshot" || serviceType == "longrun" {
-        dependenciesDir = currentServicePath + "/dependencies.d"
-    } else {
-        *lints = append(*lints, Lint{
-            Service: currentService,
-            Message: fmt.Sprintf("service type (%s) in type file for %s does not exist", serviceType, currentService),
-        })
-        return false
-    }
+	// check if the run file exists
+	if serviceType == "longrun" {
+		runFile := currentServicePath + "/run"
+		if _, err := os.Stat(runFile); os.IsNotExist(err) {
+			*lints = append(*lints, Lint{
+				Service: currentService,
+				Message: fmt.Sprintf("run file for longrun \"%s\" does not exist", currentService),
+			})
+			isValid = false
+		}
+	}
 
-    // check if the run file exists
-    if serviceType == "longrun" {
-        runFile := currentServicePath + "/run"
-        if _, err := os.Stat(runFile); os.IsNotExist(err) {
-            *lints = append(*lints, Lint{
-                Service: currentService,
-                Message: fmt.Sprintf("run file for longrun \"%s\" does not exist", currentService),
-            })
-            isValid = false
-        }
-    }
+	// check if the up file exists
+	if serviceType == "oneshot" {
+		upFile := currentServicePath + "/up"
+		if _, err := os.Stat(upFile); os.IsNotExist(err) {
+			*lints = append(*lints, Lint{
+				Service: currentService,
+				Message: fmt.Sprintf("up file for oneshot \"%s\" does not exist", currentService),
+			})
+			isValid = false
+		}
+	}
 
-    // check if the up file exists
-    if serviceType == "oneshot" {
-        upFile := currentServicePath + "/up"
-        if _, err := os.Stat(upFile); os.IsNotExist(err) {
-            *lints = append(*lints, Lint{
-                Service: currentService,
-                Message: fmt.Sprintf("up file for oneshot \"%s\" does not exist", currentService),
-            })
-            isValid = false
-        }
-    }
+	// get the dependencies
+	files, err := ioutil.ReadDir(dependenciesDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var dependencies []string
+	for _, file := range files {
+		dependencies = append(dependencies, file.Name())
+		if file.Name() == "base" {
+			// we don't want to check the base directory
+			continue
+		}
+	}
 
-    // get the dependencies
-    files, err := ioutil.ReadDir(dependenciesDir)
-    if err != nil {
-        log.Fatal(err)
-    }
-    var dependencies []string
-    for _, file := range files {
-    	dependencies = append(dependencies, file.Name())
-    	if file.Name() == "base" {
-    	    // we don't want to check the base directory
-            continue
-        }
-    }
+	// add the service to the services list
+	*services = append(*services, Service{
+		Name:         currentService,
+		Type:         serviceType,
+		Dependencies: dependencies,
+	})
 
-    // add the service to the services list
-    *services = append(*services, Service{
-        Name: currentService,
-        Type: serviceType,
-        Dependencies: dependencies,
-    })
+	for _, dependency := range dependencies {
+		if containsService(*services, dependency) || dependency == "base" {
+			continue
+		}
+		// recursive call
+		Compile(rootPath, dependency, services, lints)
+	}
 
-    for _, dependency := range dependencies {
-        if containsService(*services, dependency) || dependency == "base" {
-            continue
-        }
-        // recursive call
-        Compile(rootPath, dependency, services, lints)
-    }
-
-    return isValid
+	return isValid
 }
 
 func MermaidGraph(services []Service) string {
-    var graph string
-    graph = "```mermaid\ngraph TD;\n"
-    for _, service := range services {
-        for _, dependency := range service.Dependencies {
-            graph += fmt.Sprintf("    %s --> %s\n", service.Name, dependency)
-        }
-    }
-    graph += "```\n"
-    return graph
+	var graph string
+	graph = "```mermaid\ngraph TD;\n"
+	for _, service := range services {
+		for _, dependency := range service.Dependencies {
+			graph += fmt.Sprintf("    %s --> %s\n", service.Name, dependency)
+		}
+	}
+	graph += "```\n"
+	return graph
 }
 
-func containsService (services []Service, service string) bool {
-    for _, s := range services {
-        if s.Name == service {
-            return true
-        }
-    }
-    return false
+func containsService(services []Service, service string) bool {
+	for _, s := range services {
+		if s.Name == service {
+			return true
+		}
+	}
+	return false
 }
